c58450f82a3b3bdea2a5845dc1f946f59433c692
LeoPoldOff <43377398+LeoPoldOff@users.noreply.github.com>
2020-05-07 03:14:45 +0500

    num2buf
    + function of doing string bufer like num in ax

--- mask.asm
+++ mask.asm
@@ -1,18 +1,27 @@
 .model tiny
+
+.data
+output_msg      db      '     '
+
 .code
 org 100h
 _start:
 jmp begin
 
 print_mask proc near
-	cld
-	mov	ax, 0b800h
-	mov	es, ax
-	mov	di, 0
-	mov ah, 00h
-	mov al, 03h
-	int 10h
-	xor	ax, ax
+	push ax
+	push bx
+	push cx
+	push dx
+
+	; cld
+	; mov	ax, 0b800h
+	; mov	es, ax
+	; mov	di, 0
+	; mov ah, 00h
+	; mov al, 03h
+	; int 10h
+	; xor	ax, ax
 	mov	ah, 0Eh
 	mov al, 050h
 	stosw
@@ -49,10 +58,19 @@ print_mask proc near
 	add di, 2
 	mov al, 030h
 	stosw
+
+	push dx
+	push cx
+	push bx
+	push ax
 	ret
 print_mask endp
 
-change_point proc near ; victim seems like 999 or 099 or 009 in ax
+change_point proc near ; seems like 999 or 099 or 009 in ax
+	push bx
+	push cx
+	push dx
+
 	mov ax, 001
 	xor dx, dx
 	mov bx, 10
@@ -81,10 +99,18 @@ change_point proc near ; victim seems like 999 or 099 or 009 in ax
 	mov	ah, 0eh
 	mov al, dl
 	stosw
+
+	pop dx
+	pop cx
+	pop bx
 	ret
 change_point endp
 
 change_speed proc near ; seems like 999 or 099 or 009 in ax
+	push bx
+	push cx
+	push dx
+
 	mov ax, 300
 	xor dx, dx
 	mov bx, 10
@@ -113,12 +139,20 @@ change_speed proc near ; seems like 999 or 099 or 009 in ax
 	mov	ah, 0eh
 	mov al, dl
 	stosw
+
+	pop dx
+	pop cx
+	pop bx
 	ret
 change_speed endp
 
-int2str16onedigit proc near ; victim in bl, res in dl
-	mov cl, bl
-	mov dl, bl
+int2str16onedigit proc near ; num in al, res in al
+	push bx
+	push cx
+	push dx
+
+	mov cl, al
+	mov dl, al
 	shr cl, 4
 	and dl, 0fh
 	add cl, 30h
@@ -135,13 +169,99 @@ int2str16onedigit proc near ; victim in bl, res in dl
 @@5:
 	add dl, 7h
 @@7:
+	mov al, dl
+
+	pop dx
+	pop cx
+	pop bx
 	ret
 int2str16onedigit endp
 
+print_string                proc near
+    push    ax
+    push    cx
+    push    es
+    push    di
+    push    si
+
+    mov     ax,     0b800h
+    mov     es,     ax
+    mov     di,     660
+    xor     ax,     ax
+    mov     cx,     5
+    mov     si,     offset output_msg
+    ; mov	[si + 1],	dl	
+
+    loop_1:
+        lodsb
+        mov     ah,     0eh
+        stosw
+        loop loop_1
+
+    pop     si
+    pop     di
+    pop     es
+    pop     cx
+    pop     ax
+    ret
+print_string                endp
+
+num2buf proc near ; num in ax, res in output_msg
+    push    bx
+    push    cx
+    push 	dx
+    push    es
+    push    di
+    push    si
+
+    mov     si,     offset output_msg
+    mov cx, 5
+
+    loopn2b:
+    	cmp ax, 0
+    	xor dx, dx
+    	mov bx, 10
+    	div bx
+    	push ax
+    	mov ax, dx
+    	call int2str16onedigit
+    	pop dx
+    	push ax
+    	mov ax, dx
+    	loop loopn2b
+
+	pop ax
+	mov		[si],	ax
+	pop ax
+	mov		[si + 1],	ax
+	pop ax
+	mov		[si + 2],	ax
+	pop ax
+	mov		[si + 3],	ax
+	pop ax
+	mov		[si + 4],	ax
+
+    pop     si
+    pop     di
+    pop     es
+    pop		dx
+    pop     cx
+    pop		bx
+    ret
+num2buf endp
+
 begin proc near
-call print_mask
-call change_point
-call change_speed
+	cld
+	mov	ax, 0b800h
+	mov	es, ax
+	mov	di, 0
+	mov ah, 00h
+	mov al, 03h
+	int 10h
+	xor	ax, ax
+	mov ax, 608 ; task for num2buf
+	call num2buf
+	call print_string
 @@2:
 	xor	ah,ah
 	int	16h
