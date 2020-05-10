5384d52f17b09fe88f1d5ad89b279c1f9a219dfb
D-rection <artemiy_manakov@mail.ru>
2020-05-11 01:20:34 +0500

    move_figure
    update check position

--- check_position.asm
+++ check_position.asm
@@ -2,9 +2,9 @@ model	tiny
 .data
 
 game_field:
-	dw		0000h
-	dw		0001h
 	dw		0002h
+	dw		0000h
+	dw		0FFFFh
 	dw		0003h
 	dw		0004h
 	
@@ -30,13 +30,12 @@ game_field:
 	dw		0015h
 	dw		0016h
 	dw		0017h
-	dw		0018h
 
 current_position:
-		dw		00FCh
-		dw		00FDh
-		dw		00FEh
-		dw		0FFFFh
+		dw		0000h
+		dw		0001h
+		dw		0002h
+		dw		0003h
 	
 saved_position:
 		dw		9999h
@@ -60,72 +59,354 @@ _start:
     jmp     begin
 
 begin:
-	mov		cx,		00F2h
-	call	transform_address
-	mov		ax,		cx
-	call	num2buf
-	call	print_string
-	
-	cycle_1:			
-			xor		ah,		ah
-			int		16h									; читаем с клавиатуры символ
-			cmp		ah,		1h
-			je   	cycle_1
-			
-	mov		cx,		00F2h
-	call	move_pointer
-	call	num2buf
-	call	print_string
-	
-	cycle_2:			
-			xor		ah,		ah
-			int		16h									; читаем с клавиатуры символ
-			cmp		ah,		1h
-			je   	cycle_2
-	
-	call	check_position
-	call	num2buf
-    cycle:
-			call	print_string
-			
-			xor		ah,		ah
-			int		16h									; читаем с клавиатуры символ
-			cmp		ah,		1h
-			je   	cycle
+	mov		ax,		0
+	call	move_figure
+
+	lea		ax,		[current_position]	
     jmp     exit
 
 
+
+move_figure				proc near
+	cmp		ax,		0
+	je		mvdwn
+	cmp		ax,		1
+	je		mvlft
+	cmp		ax,		2
+	je		mvrght
+	cmp		ax,		3
+	je		mvup
+	mvdwn:
+		call	move_down
+		jmp		mvfgr
+	mvlft:
+		call	move_left
+		jmp		mvfgr
+	mvrght:
+		call	move_right
+		jmp		mvfgr
+	mvup:
+		call	move_up
+	mvfgr:
+		ret
+move_figure				endp
+
+
 ; | input
-; output_msg - input string
+; read buffers
 ; | output
-; print in console
-print_string                proc near
-    push    ax
-    push    cx
-    push    es
-    push    di
-    push    si
+; empty
+move_up					proc near
+	push	ax
+	push	bx
+	push	cx
+	push	si
+	push	di
 
-    mov     ax,     0b800h
-    mov     es,     ax
-    mov     di,     660
-    xor     ax,     ax
-    mov     cx,     11
-    mov     si,     offset output_msg
+	call	saved_configuration
 
-    loop_1:
-        lodsb
-        mov     ah,     7
-        stosw
-        loop loop_1
+	mov		cx,		4
+	lea		si,		[current_position]
+	lea		di,		[current_position]
 
-    pop     si
-    pop     di
-    pop     es
-    pop     cx
-    pop     ax
-    ret
-print_string                endp
+	loop_move_up:
+		lodsw
+
+		cmp		ax,		0FFFFh
+		je		chresup
+		
+		mov		bx,		ax
+		shr		bx,		4
+		cmp		bx,		0
+		je		resconup
+		
+		dec		bx
+		shl		bx,		4
+		shl		ax,		12
+		shr		ax,		12
+		add		ax,		bx
+		stosw
+
+		loop	loop_move_up
+			
+	chresup:
+		call	check_position
+		cmp		ax,		0
+		je		mvupret
+	resconup:
+		call	restore_configuration
+	mvupret:
+		pop		di
+		pop		si
+		pop		cx
+		pop		bx
+		pop		ax
+		ret
+move_up					endp
+
+
+; | input
+; read buffers
+; | output
+; empty
+move_down					proc near
+	push	ax
+	push	bx
+	push	cx
+	push	si
+	push	di
+
+	call	saved_configuration
+
+	mov		cx,		4
+	lea		si,		[current_position]
+	lea		di,		[current_position]
+
+	loop_move_down:
+		cmp		cx,		0
+		je		chresdown
+		dec		cx
+
+		lodsw
+
+		cmp		ax,		0FFFFh
+		je		chresdown
+		
+		mov		bx,		ax
+		shr		bx,		4
+		cmp		bx,		18h
+		je		rescondown
+		
+		inc		bx
+		shl		bx,		4
+		shl		ax,		12
+		shr		ax,		12
+		add		ax,		bx
+		stosw
+
+		jmp		loop_move_down
+			
+	chresdown:
+		call	check_position
+		cmp		ax,		0
+		je		mvdownret
+	rescondown:
+		call	restore_configuration
+	mvdownret:
+		pop		di
+		pop		si
+		pop		cx
+		pop		bx
+		pop		ax
+		ret
+move_down					endp
+
+
+; | input
+; read buffers
+; | output
+; empty
+move_right					proc near
+	push	ax
+	push	bx
+	push	cx
+	push	si
+	push	di
+
+	call	saved_configuration
+
+	mov		cx,		4
+	lea		si,		[current_position]
+	lea		di,		[current_position]
+
+	loop_move_right:
+		lodsw
+
+		cmp		ax,		0FFFFh
+		je		chresright
+		
+		mov		bx,		ax
+		shl		bx,		12
+		shr		bx,		12
+		cmp		bx,		0Fh
+		jne		increment_right
+
+		shr		ax,		4
+		shl		ax,		4
+		jmp		record_right	
+		
+		increment_right:
+			inc		ax
+		record_right:
+			stosw
+
+		loop	loop_move_right
+			
+	chresright:
+		call	check_position
+		cmp		ax,		0
+		je		mvrightret
+	resconright:
+		call	restore_configuration
+	mvrightret:
+		pop		di
+		pop		si
+		pop		cx
+		pop		bx
+		pop		ax
+		ret
+move_right					endp
+
+
+; | input
+; read buffers
+; | output
+; empty
+move_left					proc near
+	push	ax
+	push	bx
+	push	cx
+	push	si
+	push	di
+
+	call	saved_configuration
+
+	mov		cx,		4
+	lea		si,		[current_position]
+	lea		di,		[current_position]
+
+	loop_move_left:
+		lodsw
+
+		cmp		ax,		0FFFFh
+		je		chresleft
+		
+		mov		bx,		ax
+		shl		bx,		12
+		shr		bx,		12
+		cmp		bx,		0
+		jne		decrement_left
+
+		shr		ax,		4
+		shl		ax,		4
+		mov		bx,		0Fh
+		add		ax,		bx
+		jmp		record_left	
+		
+		decrement_left:
+			dec		ax
+		record_left:
+			stosw
+
+		loop	loop_move_left
+			
+	chresleft:
+		call	check_position
+		cmp		ax,		0
+		je		mvleftret
+	resconleft:
+		call	restore_configuration
+	mvleftret:
+		pop		di
+		pop		si
+		pop		cx
+		pop		bx
+		pop		ax
+		ret
+move_left					endp
+
+
+
+restore_configuration proc near	; transport saved position & configuration
+	push ax						;to current position and configuration
+	push bx
+
+	lea bx, saved_position		; link to saved_position in bx
+
+	mov ax, [bx]				; first dot from saved_position in ax
+	lea bx, current_position	; link to current_position in bx
+	mov [bx], ax 				; restore first dot from saved_configuration to current_position
+	lea bx, saved_position
+
+	mov ax, [bx + 2]			; second dot
+	lea bx, current_position
+	mov [bx + 2], ax
+	lea bx, saved_position
+
+	mov ax, [bx + 4]			; third dot 
+	lea bx, current_position
+	mov [bx + 4], ax
+	lea bx, saved_position
+
+	mov ax, [bx + 6]			; fourth dot
+	lea bx, current_position
+	mov [bx + 6], ax
+	lea bx, saved_position
+
+	lea bx, saved_rotate		; link to saved_rotate in bx
+	mov ax, [bx]				; saved_rotate in ax
+	lea bx, current_rotate		; link to current_rotate in bx
+	mov [bx], ax 				; restore current_rotate from saved_rotate
+
+	; lea bx, current_position  <==== testing
+	; mov ax, [bx]
+	; mov ax, [bx + 2]
+	; mov ax, [bx + 4]
+	; mov ax, [bx + 6]
+
+	; lea bx, current_rotate
+	; mov ax, [bx]              <==== testing
+
+
+	pop bx
+	pop ax
+	ret
+restore_configuration endp
+
+
+saved_configuration proc near 	; transport current position & configuration
+	push ax						; to saved position and configuration
+	push bx						; the same as restore_configuration but on the other side
+
+	lea bx, current_position
+
+	mov ax, [bx]
+	lea bx, saved_position
+	mov [bx], ax
+	lea bx, current_position
+
+	mov ax, [bx + 2]
+	lea bx, saved_position
+	mov [bx + 2], ax
+	lea bx, current_position
+
+	mov ax, [bx + 4]
+	lea bx, saved_position
+	mov [bx + 4], ax
+	lea bx, current_position
+
+	mov ax, [bx + 6]
+	lea bx, saved_position
+	mov [bx + 6], ax
+
+	lea bx, current_rotate
+	mov ax, [bx]
+	lea bx, saved_rotate
+	mov [bx], ax
+
+	; lea bx, saved_position  <==== testing
+	; mov ax, [bx]
+	; mov ax, [bx + 2]
+	; mov ax, [bx + 4]
+	; mov ax, [bx + 6]
+
+	; lea bx, saved_rotate
+	; mov ax, [bx]            <==== testing
+
+	pop bx
+	pop ax
+	ret
+saved_configuration 	endp
 
 
 ; | input
@@ -137,66 +418,60 @@ check_position				proc near
 	push	cx
 	push	si
 	
-	xor		ax,		ax
-	xor		cx,		cx
 	xor		bx,		bx
 	lea		si,		[current_position]
 	checker_loop:									;	Для каждой клеточки поля
 		cmp		bx,		4						
-		jge		checker_ret
-		inc		bx
-		
-		lodsw	
+		jge		checker_true
+		inc		bx	
+		lodsw
+		cmp		ax,		0FFFFh						;	Если дальше то
+		je		checker_true
 		mov		cx,		ax
-		mov		ax,		0							;	Предполагаем, что всё-таки нам не повезло и клеточка заезжает на поле
-		cmp		cx,		0FFFFh						;	Если дальше то
-		je		checker_ret
-		
 		call	move_pointer						;	Получаем соответствующую точке строку игрового поля
-		
 		call	transform_address					;	Получаем удобный для сравнения адрес точки
-		and		cx,		ax
-		
-		mov		ax,		1
-		cmp		cx,		0
-		jne		checker_ret							;	Не повезло - выходим
-		xor		ax,		ax
+		and		al,		cl
+		and		ah,		ch
+		cmp		ax,		0
+		jne		checker_false							;	Не повезло - выходим
 		jmp		checker_loop						;	Продолжаем работу
-	
+
+	checker_true:
+		mov		ax,		0
+		jmp		checker_ret
+	checker_false:
+		mov		ax,		1
 	checker_ret:
 		pop		si
 		pop		cx
 		pop		bx
-	ret
+		ret
 check_position				endp
 
 
+
+
+
 ; | input
-; cx - address
+; ax - address
 ; | output
 ; ax - value in game_field
 move_pointer			proc near
 	push	bx
 	push	si
-	
 	lea		si,		[game_field]					;	Вытаскиваем адрес игрового поля
-	mov		bx,		cx
+	mov		bx,		ax
 	shr		bx,		4								;	Младшие байты отвечают за положение внутри строчки
-	
-	move_pointer_loop:
-		cmp		bx,		0
-		je		move_pointer_ret
-		lea		si,		[si + 2]					;	Сдвигаем на два байта указатель, столько раз, сколько надо
-		dec		bx
-		jmp		move_pointer_loop
-		
-	move_pointer_ret:
-		lodsw										;	Читаем из буфера
-		pop		si
-		pop		bx
-		ret
-		
+	mov		ax,		0
+	mov		al,		2
+	mul		bx
+	add		si,		ax	
+	lodsw											;	Читаем из буфера
+	pop		si
+	pop		bx
+	ret
 move_pointer			endp
+
 
 
 ; | input
@@ -205,102 +480,29 @@ move_pointer			endp
 ; cx - transformed address
 transform_address		proc near
 	push	dx
+	push	bx
 	
 	shl		cx,		12
 	shr		cx,		12
 	
 	xor		dx,		dx
-	mov 	dx,		1000000000000000b				;	Для успешного использования операции AND, нужно инвертировать позицию
+	mov 	dx,		8000h							;	Для успешного использования операции AND, нужно инвертировать позицию
 	
 	loop_addr_transform:
+		cmp		cx,		0
+		je		tradret
+		dec		cx
+
 		shr		dx,		1
-		loop	loop_addr_transform
+		jmp	loop_addr_transform
 	
+	tradret:
 	mov		cx,		dx	
-	addr_tranform_ret:
-		pop		dx
-		ret
-transform_address		endp
 
-
-; | input
-; ax - number
-; | output
-; output_msg
-num2buf proc near
-    push    bx
-    push    cx
-    push 	dx
-    push    es
-    push    di
-    push    si
-
-    mov     si,     offset output_msg
-    mov cx, 5
-
-    loopn2b:
-    	cmp ax, 0
-    	xor dx, dx
-    	mov bx, 10
-    	div bx
-    	push ax
-    	mov ax, dx
-    	call int2str16onedigit
-    	pop dx
-    	push ax
-    	mov ax, dx
-    	loop loopn2b
-
-	pop ax
-	mov		[si],	ax
-	pop ax
-	mov		[si + 1],	ax
-	pop ax
-	mov		[si + 2],	ax
-	pop ax
-	mov		[si + 3],	ax
-	pop ax
-	mov		[si + 4],	ax
-
-    pop     si
-    pop     di
-    pop     es
-    pop		dx
-    pop     cx
-    pop		bx
-    ret
-num2buf endp
-
-int2str16onedigit proc near ; num in al, res in al
-	push bx
-	push cx
-	push dx
-
-	mov cl, al
-	mov dl, al
-	shr cl, 4
-	and dl, 0fh
-	add cl, 30h
-	add dl, 30h
-	cmp cl, 39h
-	jg @@4
-@@6:
-	cmp dl, 39h
-	jg @@5
-	jmp @@7
-@@4:
-	add cl, 7h
-	jmp @@6
-@@5:
-	add dl, 7h
-@@7:
-	mov al, dl
-
-	pop dx
-	pop cx
-	pop bx
+	pop		bx
+	pop		dx
 	ret
-int2str16onedigit endp
+transform_address		endp
 
 
 exit:
