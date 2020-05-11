09a023a83a40cc513b4dd7db2262d37955f6fdf1
AgaFFon <43129104+AgaFFon@users.noreply.github.com>
2020-05-11 18:09:36 +0500

    Отрисовка #2
    Теперь с цветами и current_position'ами

--- glass.asm
+++ glass.asm
@@ -1,4 +1,5 @@
-.model tiny
+model tiny
+.386
 .code
 org 100h
 
@@ -7,7 +8,10 @@ begin:
     nop
 _start:
 
+    ;call    draw_field
+    call draw_glass
     call    draw_field
+    call draw_cur_pos
 
 
 @@2:
@@ -19,11 +23,11 @@ _start:
 
 
 game_field:
-	    dw		1111h
-	    dw		6666h
-	    dw		2222h
-	    dw		3333h
-	    dw		2222h
+	    dw		0000h
+		dw		0000h
+		dw		0020h
+		dw		0030h
+	    dw		0FFFFh
 	    dw		1111h
 	    dw		4444h
 	    dw		5555h
@@ -43,9 +47,6 @@ game_field:
 	    dw		3333h
 	    dw		2222h
 	    dw		1111h
-        
-
-
 
 
 ScreenClear proc near                       ; полная чистка экрана
@@ -101,13 +102,134 @@ draw_glass proc near                         ; рисуем стакан
 draw_glass endp
 
 
+current_position:
+		dw		0017h
+		dw		0027h
+		dw		0018h
+		dw		0028h
+
+
+current_color	dw 		3
+; 1 - red
+; 2 - brown
+; 3 - yellow
+; 4 - green
+; 5 - blue
+; 6 - purple
+; 7 - cyan
+
+
+color_choice proc near                          ; выбирает цвет для current_position
+        push    bx                              ; использует current_color
+        lea     bx,     current_color
+        mov     ax,     [bx]
+
+        cmp     ax,     1
+        je      _red
+        cmp     ax,     2
+        je      _brown
+        cmp     ax,     3
+        je      _yellow
+        cmp     ax,     4
+        je      _green
+        cmp     ax,     5
+        je      _blue
+        cmp     ax,     6
+        je      _purple
+        cmp     ax,     7
+        je      _cyan
+
+    _red:
+        mov     ah,     04h
+        jmp     _go_on
+    _brown:
+        mov     ah,     06h
+        jmp     _go_on
+    _yellow:
+        mov     ah,     0eh
+        jmp     _go_on
+    _green:
+        mov     ah,     02h
+        jmp     _go_on
+    _blue:
+        mov     ah,     01h
+        jmp     _go_on
+    _purple:
+        mov     ah,     05h
+        jmp     _go_on
+    _cyan:
+        mov     ah,     03h
+        jmp     _go_on
+    
+    _go_on:
+        pop     bx
+        ret
+color_choice endp
+
+
+draw_cur_pos proc near                       ; рисует текущую фигуру
+        push    ax                           ; использует current_position и current_color
+        push    bx
+        push    cx
+        push    dx
+
+        lea     bx,     current_position
+        mov     cx,     4
+    _loop_cur:              
+        mov     ax,     [bx]
+        cmp     ax,     0FFFFh
+        je      _ret
+
+        push    cx
+        push    ax
+        shr     ax,     4
+        mov     cx,     ax
+        mov     ax,     160
+        mul     cx
+        add     ax,     46
+        mov     cx,     ax                          ; в cx - координата 
+
+        pop     ax
+        shl     ax,     12
+        shr     ax,     12
+        push    cx
+        mov     cx,     4
+        mul     cx
+        pop     cx                      ; т.к. квадрат - это 2 клетки
+        add     cx,     ax
+        
+        mov     dx,     0b800h          ; место указано в dx
+        mov     es,     dx
+
+        call    color_choice            ; выбор цвета в отдельной ф-ции
+                                        ; (иначе ругается на слишком длинные джампы)
+        mov     al,     0DBh
+        mov     di,     cx
+        stosw
+        stosw
+        
+        pop     cx
+        add     bx,     2
+        loop    _loop_cur
+    _ret:
+        pop     dx
+        pop     cx
+        pop     bx
+        pop     ax
+        ret
+draw_cur_pos endp
+
+
+
+
+
 draw_field proc near                                  ; рисует игровое поле
         push    ax                              ; использует draw_str и drawer
         push    bx
         push    cx
         push    dx                               
         cld
-        call    draw_glass                      ; вызов отрисовки стакана (можно убрать в любое другое место)
+        ;call    draw_glass                      ; вызов отрисовки стакана (можно убрать в любое другое место)
 
         mov     dx,     46                      ; отрисовка первой строки
         lea     bx,     game_field              
